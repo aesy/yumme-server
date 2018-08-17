ef55e4c34516a5555fadb283610a6f3c234f0c13
Isak W <isak.wertwein@gmail.com>
2018-08-17 20:41:12 +0200

    Add user and auth related tables

---
+++ src/main/resources/db/migration/201710132056__User.sql
@@ -0,0 +1,88 @@
+CREATE TABLE `user` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `email` varchar(64) NOT NULL UNIQUE,
+    `password` varchar(64) NOT NULL,
+    `created_at` int unsigned NOT NULL DEFAULT 0,
+    `modified_at` int unsigned NOT NULL DEFAULT 0,
+    `deleted_at` int unsigned NOT NULL DEFAULT 0,
+    `suspended_at` int unsigned NOT NULL DEFAULT 0,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `user`
+    ADD INDEX (`email`),
+    ADD INDEX (`email`, `password`);
+
+DELIMITER $$
+CREATE TRIGGER `trig_user_insert` BEFORE INSERT ON `user`
+FOR EACH ROW
+    BEGIN
+        IF (new.`created_at` = 0) THEN
+            SET new.`created_at` = UNIX_TIMESTAMP();
+        END IF;
+
+        IF (new.`modified_at` = 0) THEN
+            SET new.`modified_at` = UNIX_TIMESTAMP();
+        END IF;
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER `trig_user_update` BEFORE UPDATE ON user
+FOR EACH ROW
+    BEGIN
+        IF (new.`modified_at` = 0) THEN
+            SET new.`modified_at` = UNIX_TIMESTAMP();
+        END IF;
+    END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `proc_user_is_deleted` (IN `user_id` int unsigned)
+BEGIN
+    SELECT
+        EXISTS(
+            SELECT *
+            FROM `user`
+            WHERE `id` = `user_id` AND `deleted_at` > 0
+        );
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `proc_user_is_suspended` (IN `user_id` int unsigned)
+BEGIN
+    SELECT
+        EXISTS(
+            SELECT *
+            FROM `user`
+            WHERE `id` = `user_id` AND `suspended_at` > 0
+        );
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `proc_user_delete` (IN `user_id` int unsigned)
+BEGIN
+    UPDATE `user`
+    SET `deleted_at` = UNIX_TIMESTAMP()
+    WHERE `id` = `user_id`;
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `proc_user_suspend` (IN `user_id` int unsigned)
+BEGIN
+    UPDATE `user`
+    SET `suspended_at` = UNIX_TIMESTAMP()
+    WHERE `id` = `user_id`;
+END $$
+DELIMITER ;
+
+ALTER TABLE `recipe`
+    ADD COLUMN `author` int unsigned NOT NULL,
+    ADD INDEX (`author`),
+    ADD CONSTRAINT
+        FOREIGN KEY (`author`) REFERENCES `user` (`id`)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE;
---
+++ src/main/resources/db/migration/201909042105__Role_Permission.sql
@@ -0,0 +1,56 @@
+CREATE TABLE `role` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `role` (`name`)
+    VALUES ('ADMIN'), ('USER');
+
+CREATE TABLE `permission` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(64) NOT NULL UNIQUE,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_has_role` (
+    `user` int unsigned NOT NULL,
+    `role` int unsigned NOT NULL,
+    `assigned_at` INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`user`, `role`),
+    FOREIGN KEY (`user`) REFERENCES `user` (`id`)
+        ON UPDATE CASCADE,
+    FOREIGN KEY (`role`) REFERENCES `role` (`id`)
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER $$
+CREATE TRIGGER `trig_user_has_role_insert` BEFORE INSERT ON `user_has_role`
+FOR EACH ROW
+    BEGIN
+        IF (new.`assigned_at` = 0) THEN
+            SET new.`assigned_at` = UNIX_TIMESTAMP();
+        END IF;
+    END $$
+DELIMITER ;
+
+CREATE TABLE `role_grants_permission` (
+    `role` int unsigned NOT NULL,
+    `permission` int unsigned NOT NULL,
+    `assigned_at` INT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY (`role`, `permission`),
+    FOREIGN KEY (`role`) REFERENCES `role` (`id`)
+        ON UPDATE CASCADE,
+    FOREIGN KEY (`permission`) REFERENCES `permission` (`id`)
+        ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DELIMITER $$
+CREATE TRIGGER `trig_role_grants_permission_insert` BEFORE INSERT ON `role_grants_permission`
+FOR EACH ROW
+    BEGIN
+        IF (new.`assigned_at` = 0) THEN
+            SET new.`assigned_at` = UNIX_TIMESTAMP();
+        END IF;
+    END $$
+DELIMITER ;
