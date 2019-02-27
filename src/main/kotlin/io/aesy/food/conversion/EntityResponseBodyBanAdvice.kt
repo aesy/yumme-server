4080b81c380108ce0f6beb1091deb9c1fb3d7bbf
Isak W <isak.wertwein@gmail.com>
2019-02-27 23:31:55 +0100

    Ban database entities as response bodies (previously commented out)

--- src/main/kotlin/io/aesy/food/conversion/EntityResponseBodyBanAdvice.kt
+++ src/main/kotlin/io/aesy/food/conversion/EntityResponseBodyBanAdvice.kt
@@ -1,35 +1,58 @@
 package io.aesy.food.conversion
 
-//val log: Logger = LoggerFactory.getLogger(EntityResponseBodyBanAdvice::class.java)
-//
-//@ControllerAdvice
-//@Order(Int.MAX_VALUE)
-//class EntityResponseBodyBanAdvice(
-//    private val modelMapper: ModelMapper
-//): AbstractMappingJacksonResponseBodyAdvice() {
-//    override fun beforeBodyWriteInternal(
-//        bodyContainer: MappingJacksonValue,
-//        contentType: MediaType,
-//        returnType: MethodParameter,
-//        request: ServerHttpRequest,
-//        response: ServerHttpResponse
-//    ) {
-//        val value = bodyContainer.value
-//
-//        val hasEntity = when (value) {
-//            is Page<*> -> value.any { it::class.java.isAnnotationPresent(Entity::class.java) }
-//            is Collection<*> -> value.stream().anyMatch { it!!::class.java.isAnnotationPresent(Entity::class.java) }
-//            else -> bodyContainer.value::class.java.isAnnotationPresent(Entity::class.java)
-//        }
-//
-//        if (!hasEntity) {
-//            return
-//        }
-//
-//        log.error("A response containing a @Entity object detected. " +
-//                  "The response has been set to status 500. " +
-//                  "Prefer a custom DTO object.")
-//        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR)
-//        bodyContainer.value = Unit
-//    }
-//}
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.core.MethodParameter
+import org.springframework.core.annotation.Order
+import org.springframework.data.domain.Page
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.MappingJacksonValue
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice
+import javax.persistence.Entity
+
+@ControllerAdvice
+@Order(Int.MAX_VALUE)
+class EntityResponseBodyBanAdvice: AbstractMappingJacksonResponseBodyAdvice() {
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(EntityResponseBodyBanAdvice::class.java)
+    }
+
+    override fun beforeBodyWriteInternal(
+        bodyContainer: MappingJacksonValue,
+        contentType: MediaType,
+        returnType: MethodParameter,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse
+    ) {
+        val value = bodyContainer.value
+
+        val hasEntity = when (value) {
+            is Page<*> -> value.any {
+                it::class.java.isAnnotationPresent(Entity::class.java)
+            }
+            is Collection<*> -> value.stream().anyMatch {
+                if (it == null) {
+                    return@anyMatch false
+                }
+
+                it::class.java.isAnnotationPresent(Entity::class.java)
+            }
+            else -> bodyContainer.value::class.java.isAnnotationPresent(Entity::class.java)
+        }
+
+        if (!hasEntity) {
+            return
+        }
+
+        logger.error(
+            "A response containing a @Entity object detected. " +
+            "The response has been set to status 500. " +
+            "Use a custom DTO object to avoid leaking data by accident.")
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR)
+        bodyContainer.value = Unit
+    }
+}
