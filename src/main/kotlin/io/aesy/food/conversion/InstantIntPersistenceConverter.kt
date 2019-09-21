8e4cb6576d562b6b71236a23d63cd9875241d0f0
Isak W <isak.wertwein@gmail.com>
2019-09-22 00:23:07 +0200

    Add common custom jackson converters

---
+++ src/main/kotlin/io/aesy/food/conversion/InstantIntPersistenceConverter.kt
@@ -0,0 +1,22 @@
+package io.aesy.food.conversion
+
+import java.time.Instant
+import javax.persistence.AttributeConverter
+
+class InstantIntPersistenceConverter: AttributeConverter<Instant, Long> {
+    override fun convertToDatabaseColumn(instant: Instant?): Long? {
+        if (instant == null) {
+            return 0
+        }
+
+        return instant.epochSecond
+    }
+
+    override fun convertToEntityAttribute(seconds: Long?): Instant? {
+        if (seconds == null) {
+            return Instant.ofEpochSecond(0)
+        }
+
+        return Instant.ofEpochSecond(seconds)
+    }
+}
