696b1237e3c06f95af03c4efc56eb4299da32418
Isak W <isak.wertwein@gmail.com>
2021-03-23 21:43:00 +0100

    Clean up application annotation usage

--- src/main/kotlin/io/aesy/yumme/Application.kt
+++ src/main/kotlin/io/aesy/yumme/Application.kt
@@ -3,19 +3,7 @@ package io.aesy.yumme
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.EnableAspectJAutoProxy
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.scheduling.annotation.EnableAsync
-import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.transaction.annotation.EnableTransactionManagement
 
-@EnableAsync
-@EnableAspectJAutoProxy
-@EnableCaching
-@EnableScheduling
-@EnableJpaRepositories
-@EnableTransactionManagement(proxyTargetClass = true)
 @SpringBootApplication
 class YummeApplication
 
