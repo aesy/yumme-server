f529839552eb33f80cd79b05393f68cb0e79078c
Isak W <isak.wertwein@gmail.com>
2021-03-23 21:10:00 +0100

    Add test for collection persistence

---
+++ src/test/kotlin/io/aesy/yumme/repository/CollectionRepositoryPersistenceTest.kt
@@ -0,0 +1,42 @@
+package io.aesy.yumme.repository
+
+import io.aesy.test.TestType
+import io.aesy.yumme.entity.Collection
+import io.aesy.yumme.entity.User
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import strikt.api.expectThat
+import strikt.assertions.*
+
+@TestType.Persistence
+class CollectionRepositoryPersistenceTest {
+    @Autowired
+    private lateinit var collectionRepository: CollectionRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun `It should be possible to persist a collection`() {
+        val user = User(email = "test@test.com", password = "secret")
+        userRepository.save(user)
+
+        val collection = Collection(title = "woop", owner = user)
+        collectionRepository.save(collection)
+
+        expectThat(collection.id).isNotNull()
+    }
+
+    @Test
+    fun `It should be possible to fetch collections by owner`() {
+        val user = User(email = "test@test.com", password = "secret")
+        userRepository.save(user)
+
+        val collection = Collection(title = "woop", owner = user)
+        collectionRepository.save(collection)
+
+        val collections = collectionRepository.findByOwner(user)
+
+        expectThat(collections).map(Collection::id).containsExactly(collection.id)
+    }
+}
