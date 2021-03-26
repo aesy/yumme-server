4f4e5233b6404f90901df4b89ed72421ee3c5239
Isak W <isak.wertwein@gmail.com>
2021-03-26 18:12:00 +0100

    Speed up test by reusing database instance
    All tables are dropped and recreated between test method calls

--- src/test/kotlin/io/aesy/test/MariaDBExtension.kt
+++ src/test/kotlin/io/aesy/test/MariaDBExtension.kt
@@ -1,27 +1,69 @@
 package io.aesy.test
 
+import org.flywaydb.core.Flyway
 import org.junit.jupiter.api.extension.*
+import org.junit.jupiter.api.extension.ExtensionContext.Namespace
+import org.springframework.beans.factory.getBean
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.testcontainers.containers.MariaDBContainer
+import java.util.*
 
-class MariaDBExtension: BeforeAllCallback, AfterAllCallback {
+class MariaDBExtension:
+    BeforeAllCallback, BeforeEachCallback, AfterEachCallback, ExtensionContext.Store.CloseableResource {
 
-    private lateinit var container: MariaDBContainer<Nothing>
-
-    override fun beforeAll(context: ExtensionContext) {
-        container = MariaDBContainer<Nothing>("mariadb:10").apply {
-            withDatabaseName("yumme-test")
-            withUsername("yumme-test")
-            withPassword("yumme-test")
-        }
-
-        container.start()
-
-        System.setProperty("spring.datasource.url", container.jdbcUrl)
-        System.setProperty("spring.datasource.username", container.username)
-        System.setProperty("spring.datasource.password", container.password)
+    companion object {
+        private const val DATABASE_NAME = "yumme-test"
+        private var container: MariaDBContainer<Nothing>? = null
     }
 
-    override fun afterAll(context: ExtensionContext) {
-        container.stop()
+    @Synchronized
+    override fun beforeAll(context: ExtensionContext) {
+        if (container == null) {
+            container = create()
+
+            // Override application.yml
+            System.setProperty("spring.datasource.url", container!!.jdbcUrl)
+            System.setProperty("spring.datasource.username", container!!.username)
+            System.setProperty("spring.datasource.password", container!!.password)
+
+            // The following line registers a callback hook when the root test context is shut down
+            context.root.getStore(Namespace.GLOBAL).put(MariaDBExtension::class.qualifiedName, this)
+        }
+    }
+
+    override fun beforeEach(context: ExtensionContext) {
+        getSpringContext(context).ifPresent(::migrate)
+    }
+
+    override fun afterEach(context: ExtensionContext) {
+        getSpringContext(context).ifPresent(::clean)
+    }
+
+    override fun close() {
+        container?.stop()
+    }
+
+    private fun getSpringContext(context: ExtensionContext): Optional<ApplicationContext> = try {
+        Optional.of(SpringExtension.getApplicationContext(context))
+    } catch (e: IllegalArgumentException) {
+        Optional.empty()
+    }
+
+    private fun create(): MariaDBContainer<Nothing> = MariaDBContainer<Nothing>("mariadb:10").apply {
+        withDatabaseName(DATABASE_NAME)
+        withUsername("test")
+        withPassword("test")
+        start()
+    }
+
+    private fun clean(context: ApplicationContext) {
+        val flyway = context.getBean<Flyway>()
+        flyway.clean()
+    }
+
+    private fun migrate(context: ApplicationContext) {
+        val flyway = context.getBean<Flyway>()
+        flyway.migrate()
     }
 }
