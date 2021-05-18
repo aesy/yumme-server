a11ddbe50c887309c1f8ce023f499765ba92cf6c
Isak W <isak.wertwein@gmail.com>
2021-05-18 17:42:00 +0200

    Add ingredient validation

--- src/main/kotlin/io/aesy/yumme/dto/IngredientDto.kt
+++ src/main/kotlin/io/aesy/yumme/dto/IngredientDto.kt
@@ -1,9 +1,11 @@
 package io.aesy.yumme.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.hibernate.validator.constraints.Length
 
 @Dto
 class IngredientDto(
     @JsonProperty
+    @Length(min = 1, max = 64)
     var name: String
 )
